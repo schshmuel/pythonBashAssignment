3fa33d2dc0d980060c1cb6bad6bbe64ef9cfb49d
Shmuel Sch <shmuel@gmail.com>
2021-02-08 23:53:25 +0200

    Added bash script

---
+++ bashScript/find_10_largest_files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+####Run script with: ./find_10_largest_files.sh -d /opt
+
+get_input_dir(){
+	local OPTIND
+	while getopts ":d:" flag;
+	do
+   		case "$flag" in
+        		d) DIR=$OPTARG;;
+    	esac
+	done
+}
+
+check_if_dir_empty(){
+
+echo "ROOT DIR: $DIR"
+if [ -z "${DIR}" ]; then
+    echo "DIR is unset or set to the empty string"
+    exit 1
+fi
+}
+
+###Main###
+get_input_dir "$@"
+check_if_dir_empty
+
+sudo find $DIR -mount -type f -printf '%s %p\n' 2>/dev/null| sort -nr | head -10
