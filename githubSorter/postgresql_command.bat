12b98c849d997047774faafceafb9ad410d4576d
Shmuel Sch <shmuel@gmail.com>
2021-02-11 01:00:40 +0200

    Update db connection

--- githubSorter/postgresql_command.bat
+++ githubSorter/postgresql_command.bat
@@ -8,3 +8,5 @@ helm install my-release ^
 kubectl wait --for=condition=Ready pod/my-release-postgresql-0
 
 kubectl exec -it my-release-postgresql-0 -- sh -c "export PGPASSWORD=secret   && psql -U postgres -d my-database -c 'create table github (repository_name varchar primary key, stars serial, primary_language varchar)'"
+
+kubectl exec -it my-release-postgresql-0 -- sh -c "export PGPASSWORD=secret   && psql -U postgres -d my-database -c 'select * from github'"
--- githubSorter/src/__main__.py
+++ githubSorter/src/__main__.py
@@ -2,29 +2,63 @@ import os
 import sys
 
 from github import Github, Repository
+import psycopg2
+
+
+def connect_postgresql_db(db_host: str, db_name: str, db_user: str, db_password: str):
+    conn = psycopg2.connect(host=db_host,
+                            database=db_name,
+                            user=db_user,
+                            password=db_password)
+    return (conn, conn.cursor())
+
+
+def close_db_connection(conn, curse):
+    conn.close()
+    curse.close()
 
 
 def find_primary_language(repo: Repository) -> str:
     languages = repo.get_languages()
     if not languages:
-        return None
+        return 'NULL'
     else:
         v = list(languages.values())
         k = list(languages.keys())
         return k[v.index(max(v))]
 
 
-def main(access_token: str):
+def find_repositories(access_token: str):
     g = Github(access_token)
     # Notice, there is one to one connection between repo with org:kubernetes and user:kubernetes
-    repositories = g.search_repositories(query='user:kubernetes', sort='stars', order='desc')
+    return g.search_repositories(query='user:kubernetes', sort='stars', order='desc')
+
+
+def insert_to_db(conn, curse, table: str, insert_values: list):
+    sql = f"INSERT INTO {table} VALUES(%s,%s,%s)"
+    curse.executemany(sql, insert_values)
+    conn.commit()
+
+
+def loop_over_repos(repositories) -> list:
+    insert_values = []
     for repo in repositories:
         primary_language = find_primary_language(repo)
         print(f"{repo.full_name} has {repo.stargazers_count} stargazers and primary_language is {primary_language}")
         sys.stdout.flush()
+        insert_values.append((repo.full_name, str(repo.stargazers_count), primary_language))
+    return insert_values
 
 
 if __name__ == '__main__':
     access_token = os.getenv('GITHUB_ACCESS_TOKEN')
-    print(access_token)
-    main(access_token)
+    db_host = os.getenv('DB_HOST')
+    db_name = os.getenv('DB_NAME')
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    conn, curse = connect_postgresql_db(db_host, db_name, db_user, db_password)
+    repositories = find_repositories(access_token)
+    insert_values = loop_over_repos(repositories)
+    table = "github"
+    insert_to_db(conn, curse, table, insert_values)
+    close_db_connection(conn, curse)
